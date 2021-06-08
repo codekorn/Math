fd8d01d87cb62dd7951da4825f2f8793595aa9d3
Menachi Korn <codekorn@gmail.com>
2021-06-08 18:24:00 +0300

    Added static

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from pages.views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', Home_page_view.as_view(), name='home'),
     path('about/', About_page_view.as_view(), name='about'),
 ]
+urlpatterns += staticfiles_urlpatterns()
