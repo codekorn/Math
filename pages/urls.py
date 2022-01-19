fe7719f7f7447ef1d03a1968bf7d4662fc0a5a3d
codekorn <codekorn@gmail.com>
2022-01-19 12:35:10 -0400

    Deleted pages

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from pages.views import *
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', Home_page_view.as_view(), name='home'),
-    path('about/', About_page_view.as_view(), name='about'),
-]
